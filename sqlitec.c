17770daa20563d7962bd0f810c63bb13fd3e4df5
dev-xero <elo48858@gmail.com>
2024-05-11 01:15:06 +0100

    feat: Implement meta and statement proto handling

--- sqlitec.c
+++ sqlitec.c
@@ -4,6 +4,27 @@
 #include <stdio.h>
 #include <string.h>
 
+/* Define an enum for meta command results */
+typedef enum
+{
+    META_COMMAND_SUCCESS,
+    META_COMMAND_UNRECOGNIZED
+} MetaCommandResult;
+
+/* State enums */
+typedef enum
+{
+    PREPARE_SUCCESS,
+    PREPARE_UNRECOGNIZED
+} PrepareResult;
+
+/* Statement type enums */
+typedef enum
+{
+    STATEMENT_INSERT,
+    STATEMENT_SELECT
+} StatementType;
+
 /* Define a struct for the default input buffer */
 typedef struct
 {
@@ -11,6 +32,12 @@ typedef struct
     size_t buffer_length;
     size_t input_length;
 } InputBuffer;
+
+/* Define a struct for statement information */
+typedef struct
+{
+    StatementType type;
+} Statement;
 
 /* Instantiate a new input buffer */
 InputBuffer *new_input_buffer()
@@ -47,6 +74,45 @@ void close_input_buffer(InputBuffer *input_buffer)
     free(input_buffer);
 }
 
+/* Performs a meta-command */
+MetaCommandResult do_meta_command(InputBuffer *input_buffer)
+{
+    if (strcmp(input_buffer->buffer, ".exit") == 0)
+        exit(EXIT_SUCCESS);
+    else
+        return META_COMMAND_UNRECOGNIZED;
+}
+
+/* Prepares SQL statement */
+PrepareResult prepare_statement(InputBuffer *input_buffer, Statement *statement)
+{
+    if (strncmp(input_buffer->buffer, "insert", 6) == 0)
+    { // Use strncmp() since insert will be followed by data
+        statement->type = STATEMENT_INSERT;
+        return PREPARE_SUCCESS;
+    }
+    if (strcmp(input_buffer->buffer, "select") == 0)
+    {
+        statement->type = STATEMENT_SELECT;
+        return PREPARE_SUCCESS;
+    }
+    return PREPARE_UNRECOGNIZED;
+}
+
+/* Executes statements */
+void execute_statement(Statement *statement)
+{
+    switch (statement->type)
+    {
+    case (STATEMENT_INSERT):
+        printf("Insert statement handled here.\n");
+        break;
+    case (STATEMENT_SELECT):
+        printf("Select statement handled here.\n");
+        break;
+    }
+}
+
 /** SQLiteC main */
 int main(int argc, char *argv[])
 {
@@ -57,14 +123,31 @@ int main(int argc, char *argv[])
         print_prompt();
         read_input(input_buffer);
 
-        if (strcmp(input_buffer->buffer, ".exit") == 0)
+        // meta commands, (dot commands) are handled here
+        if (input_buffer->buffer[0] == '.')
         {
-            close_input_buffer(input_buffer);
-            exit(EXIT_SUCCESS);
+            switch (do_meta_command(input_buffer))
+            {
+            case (META_COMMAND_SUCCESS):
+                continue;
+            case (META_COMMAND_UNRECOGNIZED):
+                printf("Unrecognized command '%s'\n", input_buffer->buffer);
+                continue;
+            }
         }
-        else
+
+        // SQL statements are handled here
+        Statement statement;
+        switch (prepare_statement(input_buffer, &statement))
         {
-            printf("Unrecognized command '%s'.\n", input_buffer->buffer);
+        case (PREPARE_SUCCESS):
+            break;
+        case (PREPARE_UNRECOGNIZED):
+            printf("Unrecognized keyword at start of '%s'.\n", input_buffer->buffer);
+            continue;
         }
+
+        execute_statement(&statement);
+        printf("Executed.\n");
     }
 }
